0025bdc62250ad9071a060c3bf513de6a2df20c9
Natalie-Cardot <nataliecardot@gmail.com>
2021-07-09 21:00:32 -0700

    Navbar simple toggle

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,28 +4,35 @@ import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="Coding Addict" />
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          {/* React Router DOM will be used instead of traditional links later */}
-          <ul className="links">
-            {links.map((link) => {
-              const { id, url, text } = link;
-              return (
-                <li key={id}>
-                  <a href={url}>{text}</a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        {showLinks && (
+          <div className="links-container show-container">
+            {/* React Router DOM will be used instead of traditional links later */}
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link;
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
         <ul className="social-icons">
           {social.map((socialIcon) => {
             const { id, url, icon } = social;
